212ccdab8cdd3e95abe57985f9cd047ee9d40232
Daniel Page <dwi65@uclive.ac.nz>
2019-03-19 00:36:42 -0700

    Week 4 Lab Code

---
+++ Debug/OrbitOLED/OrbitOLEDInterface.d
@@ -0,0 +1,53 @@
+# FIXED
+
+OrbitOLED/OrbitOLEDInterface.obj: ../OrbitOLED/OrbitOLEDInterface.c
+OrbitOLED/OrbitOLEDInterface.obj: C:/Users/Admin/Documents/CCS-workspace/Week4Lab/OrbitOLED/lib_OrbitOled/delay.h
+OrbitOLED/OrbitOLEDInterface.obj: C:/Users/Admin/Documents/CCS-workspace/Week4Lab/OrbitOLED/lib_OrbitOled/FillPat.h
+OrbitOLED/OrbitOLEDInterface.obj: C:/Users/Admin/Documents/CCS-workspace/Week4Lab/OrbitOLED/lib_OrbitOled/LaunchPad.h
+OrbitOLED/OrbitOLEDInterface.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/stdint.h
+OrbitOLED/OrbitOLEDInterface.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/sys/stdint.h
+OrbitOLED/OrbitOLEDInterface.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/sys/cdefs.h
+OrbitOLED/OrbitOLEDInterface.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/sys/_types.h
+OrbitOLED/OrbitOLEDInterface.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/machine/_types.h
+OrbitOLED/OrbitOLEDInterface.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/machine/_stdint.h
+OrbitOLED/OrbitOLEDInterface.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/sys/_stdint.h
+OrbitOLED/OrbitOLEDInterface.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/stdbool.h
+OrbitOLED/OrbitOLEDInterface.obj: C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_gpio.h
+OrbitOLED/OrbitOLEDInterface.obj: C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_memmap.h
+OrbitOLED/OrbitOLEDInterface.obj: C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_timer.h
+OrbitOLED/OrbitOLEDInterface.obj: C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_types.h
+OrbitOLED/OrbitOLEDInterface.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/adc.h
+OrbitOLED/OrbitOLEDInterface.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/gpio.h
+OrbitOLED/OrbitOLEDInterface.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/ssi.h
+OrbitOLED/OrbitOLEDInterface.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/sysctl.h
+OrbitOLED/OrbitOLEDInterface.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/timer.h
+OrbitOLED/OrbitOLEDInterface.obj: C:/Users/Admin/Documents/CCS-workspace/Week4Lab/OrbitOLED/lib_OrbitOled/OrbitBoosterPackDefs.h
+OrbitOLED/OrbitOLEDInterface.obj: C:/Users/Admin/Documents/CCS-workspace/Week4Lab/OrbitOLED/lib_OrbitOled/OrbitOled.h
+OrbitOLED/OrbitOLEDInterface.obj: C:/Users/Admin/Documents/CCS-workspace/Week4Lab/OrbitOLED/lib_OrbitOled/OrbitOledChar.h
+OrbitOLED/OrbitOLEDInterface.obj: C:/Users/Admin/Documents/CCS-workspace/Week4Lab/OrbitOLED/lib_OrbitOled/OrbitOledGrph.h
+
+../OrbitOLED/OrbitOLEDInterface.c: 
+C:/Users/Admin/Documents/CCS-workspace/Week4Lab/OrbitOLED/lib_OrbitOled/delay.h: 
+C:/Users/Admin/Documents/CCS-workspace/Week4Lab/OrbitOLED/lib_OrbitOled/FillPat.h: 
+C:/Users/Admin/Documents/CCS-workspace/Week4Lab/OrbitOLED/lib_OrbitOled/LaunchPad.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/stdint.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/sys/stdint.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/sys/cdefs.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/sys/_types.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/machine/_types.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/machine/_stdint.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/sys/_stdint.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/stdbool.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_gpio.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_memmap.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_timer.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_types.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/adc.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/gpio.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/ssi.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/sysctl.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/timer.h: 
+C:/Users/Admin/Documents/CCS-workspace/Week4Lab/OrbitOLED/lib_OrbitOled/OrbitBoosterPackDefs.h: 
+C:/Users/Admin/Documents/CCS-workspace/Week4Lab/OrbitOLED/lib_OrbitOled/OrbitOled.h: 
+C:/Users/Admin/Documents/CCS-workspace/Week4Lab/OrbitOLED/lib_OrbitOled/OrbitOledChar.h: 
+C:/Users/Admin/Documents/CCS-workspace/Week4Lab/OrbitOLED/lib_OrbitOled/OrbitOledGrph.h: 
---
+++ Release/subdir_vars.mk
@@ -0,0 +1,41 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../tm4c123gh6pm.cmd 
+
+C_SRCS += \
+../tm4c123gh6pm_startup_ccs.c \
+../week2_blink.c \
+../week2_blink_edited.c 
+
+C_DEPS += \
+./tm4c123gh6pm_startup_ccs.d \
+./week2_blink.d \
+./week2_blink_edited.d 
+
+OBJS += \
+./tm4c123gh6pm_startup_ccs.obj \
+./week2_blink.obj \
+./week2_blink_edited.obj 
+
+OBJS__QUOTED += \
+"tm4c123gh6pm_startup_ccs.obj" \
+"week2_blink.obj" \
+"week2_blink_edited.obj" 
+
+C_DEPS__QUOTED += \
+"tm4c123gh6pm_startup_ccs.d" \
+"week2_blink.d" \
+"week2_blink_edited.d" 
+
+C_SRCS__QUOTED += \
+"../tm4c123gh6pm_startup_ccs.c" \
+"../week2_blink.c" \
+"../week2_blink_edited.c" 
+
+
