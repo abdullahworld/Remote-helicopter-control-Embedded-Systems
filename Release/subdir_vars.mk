aa0db875ec42afb88cabe07bfaef2ceadf678138
Daniel Page <dwi65@uclive.ac.nz>
2019-03-19 00:52:22 -0700

    Week 4 Lab Code

--- Release/subdir_vars.mk
+++ Release/subdir_vars.mk
@@ -9,33 +9,57 @@ CMD_SRCS += \
 ../tm4c123gh6pm.cmd 
 
 C_SRCS += \
+../ADCdemo1.c \
+../ADCdemo2.c \
+../buttons4.c \
+../circBufT.c \
 ../tm4c123gh6pm_startup_ccs.c \
-../week2_blink.c \
-../week2_blink_edited.c 
+../uartDemo.c \
+C:/ti/TivaWare_C_Series-2.1.4.178/utils/ustdlib.c 
 
 C_DEPS += \
+./ADCdemo1.d \
+./ADCdemo2.d \
+./buttons4.d \
+./circBufT.d \
 ./tm4c123gh6pm_startup_ccs.d \
-./week2_blink.d \
-./week2_blink_edited.d 
+./uartDemo.d \
+./ustdlib.d 
 
 OBJS += \
+./ADCdemo1.obj \
+./ADCdemo2.obj \
+./buttons4.obj \
+./circBufT.obj \
 ./tm4c123gh6pm_startup_ccs.obj \
-./week2_blink.obj \
-./week2_blink_edited.obj 
+./uartDemo.obj \
+./ustdlib.obj 
 
 OBJS__QUOTED += \
+"ADCdemo1.obj" \
+"ADCdemo2.obj" \
+"buttons4.obj" \
+"circBufT.obj" \
 "tm4c123gh6pm_startup_ccs.obj" \
-"week2_blink.obj" \
-"week2_blink_edited.obj" 
+"uartDemo.obj" \
+"ustdlib.obj" 
 
 C_DEPS__QUOTED += \
+"ADCdemo1.d" \
+"ADCdemo2.d" \
+"buttons4.d" \
+"circBufT.d" \
 "tm4c123gh6pm_startup_ccs.d" \
-"week2_blink.d" \
-"week2_blink_edited.d" 
+"uartDemo.d" \
+"ustdlib.d" 
 
 C_SRCS__QUOTED += \
+"../ADCdemo1.c" \
+"../ADCdemo2.c" \
+"../buttons4.c" \
+"../circBufT.c" \
 "../tm4c123gh6pm_startup_ccs.c" \
-"../week2_blink.c" \
-"../week2_blink_edited.c" 
+"../uartDemo.c" \
+"C:/ti/TivaWare_C_Series-2.1.4.178/utils/ustdlib.c" 
 
 
