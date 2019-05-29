f422e7ec40455715be8c71caaaa9fed3754081a3
Hassan Alhujhoj <haa61@uclive.ac.nz>
2019-05-30 00:07:46 +1200

    Update display.h

--- display.h
+++ display.h
@@ -1,14 +1,18 @@
 #ifndef DISPLAY_H_
 #define DISPLAY_H_
 
-
 #include <stdint.h>
-
 
 void
 initDisplay(void);
 
 void
-displayStats(int16_t line);
+displayStats(void);
+
+int32_t
+get_timer(void)
+
+void
+set_timer(void);
 
 #endif /* DISPLAY_H_ */
