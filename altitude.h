3657b8d011e7b095eb45dedd3870e604bca14762
Daniel Page <dwi65@uclive.ac.nz>
2019-05-30 09:41:42 +1200

    Update altitude.h

--- altitude.h
+++ altitude.h
@@ -5,19 +5,24 @@
 #include <stdint.h>
 
 
-void ADCIntHandler(void);
+void
+ADCIntHandler(void);
 
 
-void initADC (void);
+void
+initADC (void);
 
 
-void ProcessAltData(void);
+void
+ProcessAltData(void);
 
 
-void initADCCircBuf(void);
+void 
+initADCCircBuf(void);
 
 
-int16_t getAlt(void);
+int16_t 
+getAlt(void);
 
 
 #endif /* ALTITUDE_H_ */
