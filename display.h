58dedd2cff12ea115ef854a48831b8d21e41fa89
Hassan Alhujhoj <haa61@uclive.ac.nz>
2019-06-02 20:36:12 +1200

    Consistency in commenting display.h

--- display.h
+++ display.h
@@ -1,30 +1,24 @@
+/*  display.h - Controls the OLED display, showing the stats of the helicopter. */
+
+/*  Contributers: Hassan Ali Alhujhoj, Abdullah Naeem and Daniel Page
+    Last modified: 1.6.2019 */
+
 #ifndef DISPLAY_H_
 #define DISPLAY_H_
 
-
-// display.h - Controls the OLED display, showing the stats of the helicopter.
-
-// Contributers: Hassan Ali Alhujhoj, Abdullah Naeem and Daniel Page
-// Last modified: 1.6.2019
-
-
 #include <stdint.h>
 
-
-// Initialise the Orbit OLED display
+/* Initialise the Orbit OLED display */
 void
 initDisplay(void);
 
-
-// Increments the timer to show the next line on the OLED screen
+/* Increments the timer to show the next line on the OLED screen */
 void
 incrementDispTimer(void);
-
 
 /* This display function will display each line depending on the timer that is updated based on sysTick
    The timer will be updated every 10ms and each timer count is equal to 10ms. Thus, the OLED will update every 40 ms. */
 void
 displayStats(void);
-
 
 #endif /* DISPLAY_H_ */
