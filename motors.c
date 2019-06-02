4794e45ccb5d3123e5b95627968ba88ff460e5ca
Hassan Alhujhoj <haa61@uclive.ac.nz>
2019-06-02 20:17:36 +1200

    Consistency of commenting style motors.c

--- motors.c
+++ motors.c
@@ -1,7 +1,7 @@
 /*  motors.c - Controls the PWM output to the motors
     Contributers: Hassan Ali Alhujhoj, Abdullah Naeem and Daniel Page
     Last modified: 1.6.2019
-    Based on pwmGen.c by by P.J. Bones UCECE
+    Based on pwmGen.c by by P.J. Bones UCECE */
 
 /*  Outputs: PC5 (PWM Main), PF1 (PWM Tail) */
 
