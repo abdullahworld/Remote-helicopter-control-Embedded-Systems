ffe87d802cfe78476da4b23eb67474e14f45067e
Hassan Alhujhoj <haa61@uclive.ac.nz>
2019-06-02 20:27:57 +1200

    Consistency in commenting style uart.h

--- uart.h
+++ uart.h
@@ -1,33 +1,26 @@
+/*  uart.h - Functions for UART serial output. */
+
+/*  Contributers: Hassan Ali Alhujhoj, Abdullah Naeem and Daniel Page
+    Last modified: 1.6.2019
+    Based on uartDemo.c by by P.J. Bones UCECE */
+
 #ifndef UART_H_
 #define UART_H_
 
-
-// uart.h - Functions for UART serial output.
-
-// Contributers: Hassan Ali Alhujhoj, Abdullah Naeem and Daniel Page
-// Last modified: 1.6.2019
-
-// Based on uartDemo.c by by P.J. Bones UCECE
-
-
-// Initialise USB_UART - 8 bits, 1 stop bit, no parity
+/* Initialise USB_UART - 8 bits, 1 stop bit, no parity */
 void
 initialiseUSB_UART(void);
 
-
-// Transmit a string via UART0
+/* Transmit a string via UART0 */
 void
 UARTSend (char *pucBuffer);
 
-
-// Forms and sends messages to the console
+/* Forms and sends messages to the console */
 void
 consoleMsg(void);
 
-
-// Sends messages to the console periodically
+/* Sends messages to the console periodically */
 void
 consoleMsgSpaced(void);
-
 
 #endif /* UART_H_ */
