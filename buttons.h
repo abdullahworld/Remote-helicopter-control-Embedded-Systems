1b7ae71d43540ca55fb16327ccf88b404dfb4625
Hassan Alhujhoj <haa61@uclive.ac.nz>
2019-06-02 20:50:17 +1200

    Consistency of commenting style buttons.h

--- buttons.h
+++ buttons.h
@@ -1,89 +1,84 @@
+/*  buttons.h - Support for a set of FOUR specific buttons on the Tiva/Orbit.
+    The buttons are:  UP and DOWN (on the Orbit daughterboard) plus
+    LEFT and RIGHT on the Tiva. */
+
+/*  Contributers: Hassan Ali Alhujhoj, Abdullah Naeem and Daniel Page
+    Last modified: 1.6.2019 */
+
+/*  Based on buttons4.c by P.J. Bones UCECE
+    Additional functions have been added */
+
 #ifndef BUTTONS_H_
 #define BUTTONS_H_
-
-
-/* buttons.h - Support for a set of FOUR specific buttons on the Tiva/Orbit.
-   The buttons are:  UP and DOWN (on the Orbit daughterboard) plus
-   LEFT and RIGHT on the Tiva. */
-
-// Contributers: Hassan Ali Alhujhoj, Abdullah Naeem and Daniel Page
-// Last modified: 1.6.2019
-
-// Based on buttons4.c by P.J. Bones UCECE
-// Additional functions have been added
-
 
 #include <stdint.h>
 #include <stdbool.h>
 
-//*****************************************************************************
-// Constants
-//*****************************************************************************
+/* Constants */
 enum butNames {UP = 0, DOWN, LEFT, RIGHT, NUM_BUTS};
 enum butStates {RELEASED = 0, PUSHED, NO_CHANGE};
-// UP button
+
+/* UP button */
 #define UP_BUT_PERIPH  SYSCTL_PERIPH_GPIOE
 #define UP_BUT_PORT_BASE  GPIO_PORTE_BASE
 #define UP_BUT_PIN  GPIO_PIN_0
 #define UP_BUT_NORMAL  false
-// DOWN button
+
+/* DOWN button */
 #define DOWN_BUT_PERIPH  SYSCTL_PERIPH_GPIOD
 #define DOWN_BUT_PORT_BASE  GPIO_PORTD_BASE
 #define DOWN_BUT_PIN  GPIO_PIN_2
 #define DOWN_BUT_NORMAL  false
-// LEFT button
+
+/* LEFT button */
 #define LEFT_BUT_PERIPH  SYSCTL_PERIPH_GPIOF
 #define LEFT_BUT_PORT_BASE  GPIO_PORTF_BASE
 #define LEFT_BUT_PIN  GPIO_PIN_4
 #define LEFT_BUT_NORMAL  true
-// RIGHT button
+
+/* RIGHT button */
 #define RIGHT_BUT_PERIPH  SYSCTL_PERIPH_GPIOF
 #define RIGHT_BUT_PORT_BASE  GPIO_PORTF_BASE
 #define RIGHT_BUT_PIN  GPIO_PIN_0
 #define RIGHT_BUT_NORMAL  true
 
 #define NUM_BUT_POLLS 3
-// Debounce algorithm: A state machine is associated with each button.
-// A state change occurs only after NUM_BUT_POLLS consecutive polls have
-// read the pin in the opposite condition, before the state changes and
-// a flag is set.  Set NUM_BUT_POLLS according to the polling rate.
 
-// *******************************************************
-// initButtons: Initialise the variables associated with the set of buttons
-// defined by the constants above.
+/*  Debounce algorithm: A state machine is associated with each button.
+    A state change occurs only after NUM_BUT_POLLS consecutive polls have
+    read the pin in the opposite condition, before the state changes and
+    a flag is set.  Set NUM_BUT_POLLS according to the polling rate. */
+
+/*  initButtons: Initialise the variables associated with the set of buttons
+    defined by the constants above. */
 void
 initButtons (void);
 
-// *******************************************************
-// updateButtons: Function designed to be called regularly. It polls all
-// buttons once and updates variables associated with the buttons if
-// necessary.  It is efficient enough to be part of an ISR, e.g. from
-// a SysTick interrupt.
+/*  updateButtons: Function designed to be called regularly. It polls all
+    buttons once and updates variables associated with the buttons if
+    necessary.  It is efficient enough to be part of an ISR, e.g. from
+    a SysTick interrupt. */
 void
 updateButtons (void);
 
-// *******************************************************
-// checkButton: Function returns the new button state if the button state
-// (PUSHED or RELEASED) has changed since the last call, otherwise returns
-// NO_CHANGE.  The argument butName should be one of constants in the
-// enumeration butStates, excluding 'NUM_BUTS'. Safe under interrupt.
+/*  checkButton: Function returns the new button state if the button state
+    (PUSHED or RELEASED) has changed since the last call, otherwise returns
+    NO_CHANGE.  The argument butName should be one of constants in the
+    enumeration butStates, excluding 'NUM_BUTS'. Safe under interrupt. */
 enum butStates
 checkButton (uint8_t butName);
 
 
-// Initialises the GPIO pin for the software reset button
+/* Initialises the GPIO pin for the software reset button */
 void
 initResetBut(void);
 
-
-// Checks to see if the up, down, left, or right button has been pushed
+/* Checks to see if the up, down, left, or right button has been pushed */
 void
 buttonPressed(void);
 
-
-// Performs a software reset when the designated button is pressed
+/* Performs a software reset when the designated button is pressed */
 void
 buttonReset(void);
 
-
-#endif /*BUTTONS_H_*/
+#endif /* BUTTONS_H_ */
