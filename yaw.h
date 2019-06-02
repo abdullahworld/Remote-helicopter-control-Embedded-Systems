4f3888166edfc3cdb4775c346c4d77c5db7a8aba
Hassan Alhujhoj <haa61@uclive.ac.nz>
2019-06-02 20:26:05 +1200

    Consistency in commenting yaw.h

--- yaw.h
+++ yaw.h
@@ -1,39 +1,31 @@
+/*  yaw.h - Controls the yaw of the helicopter */
+
+/*  Contributers: Hassan Ali Alhujhoj, Abdullah Naeem and Daniel Page
+    Last modified: 1.6.2019 */
+
 #ifndef YAW_H_
 #define YAW_H_
 
-
-// yaw.h - Controls the yaw of the helicopter
-
-// Contributers: Hassan Ali Alhujhoj, Abdullah Naeem and Daniel Page
-// Last modified: 1.6.2019
-
-
 #include <stdint.h>
 
-
-// ISR for quadrature encoding
+/* ISR for quadrature encoding */
 void
 YawIntHandler(void);
 
-
-// Initialises the GPIO pin register for yaw channels A and B
+/* Initialises the GPIO pin register for yaw channels A and B */
 void
 initYawGPIO(void);
 
-
-// Returns the yaw in degrees with respect to the reference point
+/* Returns the yaw in degrees with respect to the reference point */
 int16_t
 getYaw(void);
 
-
-// ISR for finding the yaw reference
+/* ISR for finding the yaw reference */
 void
 YawRefIntHandler(void);
 
-
-// Initialises the GPIO pin register for the yaw reference point
+/* Initialises the GPIO pin register for the yaw reference point */
 void
 initYawRef(void);
-
 
 #endif /* YAW_H_ */
